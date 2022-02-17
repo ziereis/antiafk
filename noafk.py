8304b152cc8bd1df51fea46ffbc479b368acd677
Thomas <thomasziereis@web.de>
2022-02-17 17:31:03 +0100

    123

--- noafk.py
+++ noafk.py
@@ -1,6 +1,6 @@
 from pynput.mouse import Button, Controller
 import time
-
+import random 
 mouse = Controller()
 starttime = time.time()
 
@@ -8,7 +8,4 @@ while True:
     print("Click")
     mouse.position = (100, 464)
     mouse.click(Button.left, 1)
-    time.sleep(240.0 - ((time.time() - starttime) % 240.0))
-
-
-nokickpls()
+    time.sleep(random.randint(240,510))
